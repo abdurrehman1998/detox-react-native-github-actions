bf4276469e71d1c34dae2ca1633f98dc9f95e02f
Muhammad Usman <M.UsmanQau786@gmail.com>
2021-08-26 15:48:19 +0500

    Initial commit

---
+++ detox.config.js
@@ -0,0 +1,77 @@
+module.exports = {
+  "test-runner": "mocha",
+  "runner-config": "e2e/.mocharc.json",
+  specs:
+    process.env.DETOX_EXPOSE_GLOBALS === "0" ? "e2eExplicitRequire" : "e2e",
+  behavior: {
+    init: {
+      exposeGlobals: process.env.DETOX_EXPOSE_GLOBALS === "0" ? false : true,
+    },
+  },
+  apps: {
+    "ios.release": {
+      type: "ios.app",
+      binaryPath:
+        "ios/build/Build/Products/Release-iphonesimulator/example.app",
+      build:
+        "export RCT_NO_LAUNCH_PACKAGER=true && xcodebuild -project ios/example.xcodeproj -UseNewBuildSystem=NO -scheme example -configuration Release -sdk iphonesimulator -derivedDataPath ios/build -quiet",
+    },
+    "ios.debug": {
+      type: "ios.app",
+      binaryPath: "ios/build/Build/Products/Debug-iphonesimulator/example.app",
+      build:
+        "xcodebuild -project ios/example.xcodeproj -UseNewBuildSystem=NO -scheme example -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build",
+    },
+    "android.debug": {
+      type: "android.apk",
+      binaryPath: "android/app/build/outputs/apk/debug/app-debug.apk",
+      build:
+        "cd android && gradlew app:assembleDebug assembleAndroidTest -DtestBuildType=debug && cd ..",
+    },
+    "android.release": {
+      type: "android.apk",
+      binaryPath: "android/app/build/outputs/apk/release/app-release.apk",
+      build:
+        "cd android ; gradlew assembleRelease assembleAndroidTest -DtestBuildType=release ; cd -",
+    },
+  },
+  devices: {
+    simulator: {
+      type: "ios.simulator",
+      device: {
+        type: "iPhone 11 Pro",
+      },
+    },
+    emulator: {
+      type: "android.emulator",
+      device: {
+        avdName: "Nexus-6-API-30",
+      },
+    },
+  },
+  configurations: {
+    "ios.sim.release": {
+      device: "simulator",
+      app: "ios.release",
+    },
+    "ios.sim.debug": {
+      device: "simulator",
+      app: "ios.debug",
+    },
+    "ios.none": {
+      type: "ios.none",
+      session: {
+        server: "ws://localhost:8099",
+        sessionId: "com.wix.demo.react.native",
+      },
+    },
+    "android.emu.debug": {
+      device: "emulator",
+      app: "android.debug",
+    },
+    "android.emu.release": {
+      device: "emulator",
+      app: "android.release",
+    },
+  },
+};
